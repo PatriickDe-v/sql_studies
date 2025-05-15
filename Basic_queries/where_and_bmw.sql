da24804d8f133c7e84858b65c45a15d27b4383dd
Patrick <patriick.dev@hotmail.com>
2025-05-14 22:07:19 -0300

    adicionado comando AND

---
+++ Basic_queries/where_and_bmw.sql
@@ -0,0 +1,4 @@
+/* comando END para procurar dois registros em uma consulta */ 
+SELECT * FROM `car_price`.`car_price_prediction_` WHERE Year > 2005 AND Year < 2010;
+
+/* No caso esse comando vai procurar veiculos com ano entre 2005 e 2010 */
