4096de097a3ac0987231cb007722e743bc15aea2
Patrick <patriick.dev@hotmail.com>
2025-05-15 22:03:14 -0300

    modificado o nome do arquivo

---
+++ Intermediare_queries/count_data.sql
@@ -0,0 +1,7 @@
+/* Comando para agrupar por anos os carros da tabela car_price */ 
+
+/* Primeiro vou fazer um comando mais simples, que é contar
+quantos veículos tem nessa tabela */ 
+
+SELECT COUNT(*) AS record_count FROM `car_price`.`car_price_prediction_`; 
+/* Temos 2.500 resultados */ 
